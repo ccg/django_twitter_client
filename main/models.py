4e7d7be03c301f6fb011764bc9101dcd1a88e90c
Chad Glendenin <ccg@pybrew.com>
2009-06-13 01:24:54 +0000

    User profile to hold Twitter OAuth token.
    	modified:   main/models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    oauth_token = models.TextField(blank=True, null=False)
+
+    def __unicode__(self):
+        return "<Profile for '%s'>" % user.name
+    def __str__(self):
+        return self.__unicode__()
