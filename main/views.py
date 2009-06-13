e2688e8d7775e5d77f29def384e92b4f38877160
Chad Glendenin <ccg@pybrew.com>
2009-06-13 06:52:25 +0000

    Display the Twitter-friends timeline.
    Put in some PrettyPrinter debugging stuff to see what's going on inside
    that JSON blob.
    
    I think this thing just become an ersatz Twitter client.
    
    	modified:   main/views.py
    	modified:   templates/main/index.html

--- main/views.py
+++ main/views.py
@@ -4,50 +4,53 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django_twitter_oauth_test.main.forms import TwitterForm
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
+
 from twitter_app.utils import *
 import twitter_app.oauth as oauth
+
 # FIXME refactor this http-connection stuff
 import httplib
+import simplejson
+import StringIO
+import pprint
 import logging
 
 CONSUMER = oauth.OAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET)
 
 @login_required
 def index(request):
+    profile = request.user.get_profile()
+    if not profile.can_tweet():
+        return render_to_response('main/index.html', locals(),
+                                  context_instance=RequestContext(request))
+    access_token_string = profile.oauth_token
+    access_token = oauth.OAuthToken.from_string(access_token_string)
+    conn = httplib.HTTPSConnection(SERVER)
     if request.method == "POST":
         form = TwitterForm(request.POST)
         if form.is_valid():
             tweet = request.POST.get('tweet', '')
-            tweetlen = len(tweet)
-            if tweetlen < 1 or tweetlen > 140:
-                logging.error("Tweet-length validation failed!")
-                logging.error("Tweet: '%s'" % tweet)
-                logging.error("Length: %d" % tweetlen)
-                return HttpResponse("TwitterForm validation failed!"
-                                    " Check logs.")
             logging.debug("tweet='%s'" % tweet)
-            profile = request.user.get_profile()
-            access_token_string = profile.oauth_token
-            if not access_token_string:
-                logging.error("no access token string for user '%s'" %
-                               request.user)
-                return HttpResponseServerError("missing access token string")
-            access_token = oauth.OAuthToken.from_string(access_token_string)
-            if not access_token:
-                logging.error("no access token for user '%s'" % request.user)
-                return HttpResponseServerError("access token fail")
 
-            conn = httplib.HTTPSConnection(SERVER)
             json = update_status(CONSUMER, conn, access_token, tweet)
-            conn.close()
 
-            logging.debug("JSON response: '%s'" % json)
+            logging.debug("update_status JSON response: '%s'" % json)
             # Submitted form was processed, so create a new, blank one.
             form = TwitterForm()
         else:
             logging.debug("got invalid form")
     else:
         form = TwitterForm()
+    timeline_json = friends_timeline(CONSUMER, conn, access_token)
+    timeline = simplejson.loads(timeline_json)
+    if timeline and len(timeline) > 0:
+        output = StringIO.StringIO()
+        pp = pprint.PrettyPrinter(stream=output, indent=2)
+        pp.pprint(timeline[0])
+        logging.debug("timeline:")
+        logging.debug(output.getvalue())
+        output.close()
+    conn.close()
     return render_to_response('main/index.html',
                               locals(),
                               context_instance=RequestContext(request),
