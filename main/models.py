b8293af2206c9a743f1dabcd4b592f4a481f2e9c
Chad Glendenin <ccg@pybrew.com>
2009-06-13 05:36:37 +0000

    Make sure profile gets created with User obj.
    	modified:   main/models.py

--- main/models.py
+++ main/models.py
@@ -5,7 +5,21 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     oauth_token = models.TextField(blank=True, null=False)
 
+    def can_tweet(self):
+        # Kind of a hack. If there's something in the oauth_token
+        # field, assume the user authorized us to tweet.
+        if self.oauth_token != None and self.oauth_token != '':
+            return True
+        return False
+
     def __unicode__(self):
-        return "<Profile for '%s'>" % user.name
+        return "username: '%s'" % self.user.username
     def __str__(self):
         return self.__unicode__()
+
+def create_profile(sender, instance=None, **kwargs):
+    if instance is None:
+        return
+    profile, created = UserProfile.objects.get_or_create(user=instance)
+
+models.signals.post_save.connect(create_profile, sender=User)
